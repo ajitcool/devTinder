3d0f539142ec8bab0fc1d4195ada106367d86343
ajitcool <ajitk6168@gmail.com>
2025-10-04 16:27:58 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -3,30 +3,23 @@ const app = express();
 const PORT = 3000;
 
 
+//app.use("/route",RH1,RH2,RH3,RH4,RH5);
+//RH: Route handler
+// To go one route handler to another route handler we need to pass next as argument and call this argument in route handler
 
 
-app.get("/user/:userId", (req,res)=>{
-    console.log(req.params);
-    console.log(req.query);
-    res.send({firstName:"Ajit", lastName:"Kumar"});
+app.use("/user",(req,res,next)=>{
+     next();
+    res.send("response 1");
+   
+
+},
+(req,res,next)=>{
+
+    res.send("response 2");
 })
 
-app.get("/user", (req,res)=>{
-    console.log(req.query);
-    res.send({firstName:"Ajit", lastName:"Kumar"});
-})
 
-app.post("/user", (req,res)=>{
-    res.send("Data successfully saved to the database!");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("Data successfully deleted from the database");
-})
-
-// app.use("/",(req,res)=>{
-//     res.send("Namaste Ajit");
-// });
 
 // Start the server
 app.listen(PORT, () => {
