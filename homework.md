421d8b6e0e90c0d031e51b1c2835c768f842aea2
ajitcool <ajitk6168@gmail.com>
2025-10-04 14:54:08 +0530

    playing with routes

---
+++ homework.md
@@ -0,0 +1,2 @@
+req.query (?): is sing ke jo value client se send kiya jata hai. usse me req.query ke object me store hota
+req.param:  isme value route ka req.params ke through aacess karte hai. late later database se data fetch kane ka  kam kar ske.
--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ const PORT = 3000;
 
 
 
+app.get("/user/:userId", (req,res)=>{
+    console.log(req.params);
+    console.log(req.query);
+    res.send({firstName:"Ajit", lastName:"Kumar"});
+})
+
 app.get("/user", (req,res)=>{
+    console.log(req.query);
     res.send({firstName:"Ajit", lastName:"Kumar"});
 })
 
@@ -17,9 +24,9 @@ app.delete("/user",(req,res)=>{
     res.send("Data successfully deleted from the database");
 })
 
-app.use("/",(req,res)=>{
-    res.send("Namaste Ajit");
-});
+// app.use("/",(req,res)=>{
+//     res.send("Namaste Ajit");
+// });
 
 // Start the server
 app.listen(PORT, () => {
